742710c9ec19283b822383eb82e7b9b50c470677
Thinkofdeath <thinkofdeath@spigotmc.org>
2015-02-22 12:03:44 +0000

    cmd/thinkbot: rework channel saving

--- cmd/thinkbot/command.go
+++ cmd/thinkbot/command.go
@@ -19,7 +19,6 @@ package main
 import (
 	"github.com/thinkofdeath/thinkbot"
 	"github.com/thinkofdeath/thinkbot/command"
-	"strings"
 )
 
 var (
@@ -37,15 +36,7 @@ func join(b *thinkbot.Bot, sender thinkbot.User, target, channel string) {
 	if len(channel) < 0 || channel[0] != '#' {
 		panic("invalid channel")
 	}
-	configLock.Lock()
-	defer configLock.Unlock()
-	channel = strings.ToLower(channel)
-	for _, c := range config.Channels {
-		if strings.ToLower(c) == channel {
-			return
-		}
-	}
-	config.Channels = append(config.Channels, channel)
-	saveConfig(config)
+	configLock.RLock()
+	defer configLock.RUnlock()
 	b.JoinChannel(channel)
 }
--- cmd/thinkbot/main.go
+++ cmd/thinkbot/main.go
@@ -51,25 +51,60 @@ func main() {
 		}
 
 		for event := range bot.Events {
-			switch event := event.(type) {
-			case thinkbot.Connected:
-				log.Println("Connected")
-				bot.AddMode('B')
-				configLock.RLock()
-				for _, c := range config.Channels {
-					bot.JoinChannel(c)
-				}
-				configLock.RUnlock()
-			case thinkbot.Stop:
-				if bot.Error() != nil {
-					log.Println(bot.Error())
-					time.Sleep(5 * time.Second)
-					continue
-				}
+			if processEvent(bot, event) {
 				return
-			default:
-				log.Printf("Unhandled event: %#v\n", event)
 			}
+		}
+	}
+}
+
+func processEvent(bot *thinkbot.Bot, event thinkbot.Event) (end bool) {
+	switch event := event.(type) {
+	case thinkbot.Connected:
+		log.Println("Connected")
+		bot.AddMode('B')
+		configLock.RLock()
+		for _, c := range config.Channels {
+			bot.JoinChannel(c)
+		}
+		configLock.RUnlock()
+	case thinkbot.JoinChannel:
+		joinChannel(event.Channel)
+	case thinkbot.PartChannel:
+		partChannel(event.Channel)
+	case thinkbot.Stop:
+		if bot.Error() != nil {
+			log.Println(bot.Error())
+			time.Sleep(5 * time.Second)
+			return
+		}
+		return true
+	default:
+		log.Printf("Unhandled event: %#v\n", event)
+	}
+	return
+}
+
+func joinChannel(channel string) {
+	configLock.Lock()
+	defer configLock.Unlock()
+	for _, c := range config.Channels {
+		if c == channel {
+			return
+		}
+	}
+	config.Channels = append(config.Channels, channel)
+	saveConfig(config)
+}
+
+func partChannel(channel string) {
+	configLock.Lock()
+	defer configLock.Unlock()
+	for i, c := range config.Channels {
+		if c == channel {
+			config.Channels = append(config.Channels[:i], config.Channels[i+1:]...)
+			saveConfig(config)
+			return
 		}
 	}
 }
